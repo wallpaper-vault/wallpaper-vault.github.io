a8c002a20cf88778a30c7b8236c52edfb3be8f2b
wallpaper-vault <moctemath@gmail.com>
2025-03-20 12:52:34 -0600

    Update script.js matomo

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function nextSlide() {
 
 setInterval(nextSlide, 3000);
 showSlide(currentIndex);
+<!-- Matomo -->
+
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://githuv.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/githuv.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
+<!-- End Matomo Code -->
